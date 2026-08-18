767121d8c7f7c45516fdc375071131b897e05236
sunilgurjar54319-jpg <Sunilgurjar54319@gmail.com>
2026-08-18 10:07:16 +0530

    fix device name update API

--- frontend/src/components/DeviceConnection.jsx
+++ frontend/src/components/DeviceConnection.jsx
@@ -135,7 +135,7 @@ function DeviceConnection() {
       setNameMessage("");
 
       const res = await authFetch(
-        `${API}/api/device/${DEVICE_ID}/name`,
+        "/api/device/${DEVICE_ID}/name",
         {
           method: "PUT",
 
